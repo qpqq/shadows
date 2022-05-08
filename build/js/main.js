7fc65428bd0289219e8f56fb8f0d362fd13c25dd
UserNaneless <vgripor@gmail.com>
2022-05-08 09:35:50 +0300

    comments

--- build/js/main.js
+++ build/js/main.js
@@ -1,25 +1,25 @@
 ymaps.ready(function () {
-    var myMap = new ymaps.Map("map", {
+    var myMap = new ymaps.Map("map", { //Cоздание карты
         center: [55.76, 37.64],
         zoom: 10,
         controls: ['geolocationControl', 'typeSelector', 'fullscreenControl', 'zoomControl']
     });
 
-    var geolocation = myMap.controls.get('geolocationControl');
-    myMap.options.set('dragCursor', 'pointer');
+    var geolocation = myMap.controls.get('geolocationControl'); //Кнопка геолокации
+    myMap.options.set('dragCursor', 'pointer'); //Установка курсора указателя
 
-    var plc, preset;
+    var plc, preset; //Точки маршрута и их внешний вид
 
 
-    var toLocationInput = document.getElementById('toLocation');
+    var toLocationInput = document.getElementById('toLocation'); //Поле для локации откуда
     toLocationInput.value = "";
-    var fromLocationInput = document.getElementById('fromLocation');
+    var fromLocationInput = document.getElementById('fromLocation'); //Поле для локации куда
     fromLocationInput.value = "";
 
-    var disableAutoSwap = false;
+    var disableAutoSwap = false; //Автопереключение откуда-куда
 
-    var toFromChecker = true; //true = from | false = to
-    document.onclick = function () {
+    var toFromChecker = true; //true = from | false = to, переключатель откуда-куда
+    document.onclick = function () { //Переключение откуда-куда по клику
         if (toLocationInput == document.activeElement) {
             toFromChecker = false;
         } else if (fromLocationInput == document.activeElement) {
@@ -27,16 +27,16 @@ ymaps.ready(function () {
         }
     }
 
-    var searchControl = new ymaps.control.SearchControl({
+    var searchControl = new ymaps.control.SearchControl({ //Поиск по карте
         options: {
             provider: 'yandex#search',
 
         }
     });
-    var toLocation, fromLocation = [];
+    var toLocation, fromLocation = []; //Координаты откуда и куда для отправки
 
 
-    geolocation.events.add('click', function () {
+    geolocation.events.add('click', function () {//По клику на кнопку геолокации установить позицию как откуда
         ymaps.geolocation.get({provider: 'yandex'}).then(function (result) {
             fromLocation = result.geoObjects.position;
             fromLocationInput.value = fromLocation;
@@ -44,7 +44,7 @@ ymaps.ready(function () {
     });
 
 
-    searchControl.events.add('resultselect', function (e) {
+    searchControl.events.add('resultselect', function (e) { //При выборе элемента в поиске записать его координаты в соответствии с переключателем откуда-куда
         let coords = searchControl.getResultsArray()[e.get('index')].geometry.getCoordinates();
         if (!toFromChecker) {
             toLocation = coords;
@@ -58,14 +58,14 @@ ymaps.ready(function () {
     })
 
 
-    myMap.controls.add(searchControl);
+    myMap.controls.add(searchControl); //Отображение поиска
 
-    var placemark1, placemark2;
-    myMap.events.add('click', function (e) {
+    var placemark1, placemark2; //Указатели на откуда и куда на карте
+    myMap.events.add('click', function (e) { //Получение координат на карте по клику по ней и установка указателей в соответсвтии
         var coords = e.get('coords');
 
         if (!toFromChecker) {
-            if (placemark1) {
+            if (placemark1) {//Проверяем есть ли маркер
                 placemark1.geometry.setCoordinates(coords);
             }
             // Если нет – создаем.
@@ -100,46 +100,46 @@ ymaps.ready(function () {
 
     });
 
-    var fromToLocations = [];
+    var fromToLocations = []; //Объединенный массив откуда, куда для более простой отправки
 
-    var notiClose = document.getElementsByClassName("notiClose")[0],
+    var notiClose = document.getElementsByClassName("notiClose")[0], //Для анимации "не может быть построен маршрут"
         noti = document.getElementsByClassName("noti")[0];
     notiClose.onclick = function () {
         noti.classList.add("notiPopout");
         noti.classList.remove("notiPopup")
     }
 
-    var loader = document.getElementById("loader");
+    var loader = document.getElementById("loader"); //Отображение загрузки
 
-    const connection = new WebSocket("ws://localhost/echo");
+    const connection = new WebSocket("ws://localhost/echo"); //Создание канала
 
 
     document.getElementById('button').onclick = function () {
 
         fromToLocations = [fromLocation, toLocation];
-        if (fromToLocations[0] != undefined && fromToLocations[1] != undefined) {
+        if (fromToLocations[0] != undefined && fromToLocations[1] != undefined) { //Если ни одна из точек не пустая, то можно отправить
 
             connection.send(JSON.stringify({
                 fromLocation: fromLocation,
                 toLocation: toLocation
             }));
-            loader.style.display = "block";
+            loader.style.display = "block"; //Включение загрузки
         }
     }
 
-    connection.onmessage = (e) => {
-        if (e.data != "") {
-            routeCoords = JSON.parse(e.data);
-            if(routeCoords["routeCoords"] == 727){
+    connection.onmessage = (e) => { //При получении сообщения
+        if (e.data != "") { //Если сообщение не пинг, для поддержания канала
+            routeCoords = JSON.parse(e.data); //Парсим JSON
+            if(routeCoords["routeCoords"] == 727){ //Если в routeCoords встречаем код ошибки: невозможно построить маршрут
                 
-                loader.style.display = "none";
-                noti.classList.remove("notiPopout");
+                loader.style.display = "none"; //Отключаем отображение загрузки
+                noti.classList.remove("notiPopout"); //Отображаем ошибку
                 noti.classList.add("notiPopup")
             }
             else{
-                myMap.geoObjects.removeAll();
-                for(let i = 0; i < routeCoords["routeCoords"].length; i++){
-                    if(i == 0){
+                myMap.geoObjects.removeAll(); //Чистим карту от лишнего перед построением
+                for(let i = 0; i < routeCoords["routeCoords"].length; i++){ 
+                    if(i == 0){ //Управление цветами точек на маршруте
                         preset = 'islands#blueCircleDotIcon';
                     }
                     else if(i == routeCoords["routeCoords"].length-1){
@@ -151,7 +151,7 @@ ymaps.ready(function () {
                     // !!!ТОЧКИ!!!
                     //plc = createRoutePlacemark(routeCoords["routeCoords"], i, preset);
                     //myMap.geoObjects.add(plc);
-                    if(i < routeCoords["routeCoords"].length-1){
+                    if(i < routeCoords["routeCoords"].length-1){ //Составление маршрута с учетом цвета, который зависит от затененности участка
                         var route = new ymaps.Polyline([
                             routeCoords["routeCoords"][i],
                             routeCoords["routeCoords"][i+1]
@@ -170,11 +170,10 @@ ymaps.ready(function () {
                     strokeColor: '#0066ff',
                 })*/
 
-                myMap.geoObjects.add(placemark1).add(placemark2);
+                myMap.geoObjects.add(placemark1).add(placemark2); //Отображение маркеров
                 placemark1 = placemark2 = null;
                 //myMap.geoObjects.add(route);
                 loader.style.display = "none";
-                console.log(routeCoords)
             }
             /*
             let route = new ymaps.multiRouter.MultiRoute({
@@ -197,7 +196,7 @@ ymaps.ready(function () {
             */
             
         } else {
-            connection.send("");
+            connection.send(""); //Отправление пингов для поддержания канала
         }
 
 
@@ -205,7 +204,7 @@ ymaps.ready(function () {
 
 
     var clearButton = document.getElementById("clear");
-    clearButton.onclick = function () {
+    clearButton.onclick = function () { //Работа кнопки очищения
         toLocationInput.value = fromLocationInput.value = '';
         toLocation = fromLocation = [];
         myMap.geoObjects.remove(placemark1);
@@ -214,20 +213,20 @@ ymaps.ready(function () {
         toFromChecker = true;
     }
 
-    var clearMapButton = document.getElementById("clearMap");
+    var clearMapButton = document.getElementById("clearMap"); //Работа кнопки очищения карты
     clearMapButton.onclick = function () {
         myMap.geoObjects.removeAll();
         placemark1 = placemark2 = null;
     }
 
-    var disableButton = document.getElementById("disable");
+    var disableButton = document.getElementById("disable"); //Работа кнопки переключения autoswap
     disableButton.onclick = function () {
         disableAutoSwap = !disableAutoSwap;
         this.textContent = disableAutoSwap ? "Enable AutoSwap" : "Disable AutoSwap";
     }
 
 
-    function createPlacemark(coords) {
+    function createPlacemark(coords) { //Функция создания маркера
         return new ymaps.Placemark(coords, {
             iconCaption: 'поиск...'
         }, {
@@ -236,7 +235,7 @@ ymaps.ready(function () {
         });
     }
 
-    function getAddress(coords, placemark) {
+    function getAddress(coords, placemark) { //Запись адреса в маркер
         placemark.properties.set('iconCaption', 'поиск...');
         ymaps.geocode(coords).then(function (res) {
             var firstGeoObject = res.geoObjects.get(0);
@@ -253,7 +252,7 @@ ymaps.ready(function () {
     }
 
 
-    function createRoutePlacemark(routeCoords, i, preset){
+    function createRoutePlacemark(routeCoords, i, preset){ //Создание точек на маршруте
         return new ymaps.Placemark(routeCoords[i], {
         }, {
             preset: preset
@@ -261,16 +260,7 @@ ymaps.ready(function () {
     }
 
 
-    function getRandomColor() {
-      let letters = '0123456789ABCDEF';
-      let color = '#';
-      for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-      }
-      return color;
-    }
-
-    function getColorFromShading(routeShading){
+    function getColorFromShading(routeShading){ //Конвертация затененности в цвет
         if(routeShading >= 0 && routeShading < .2){
             return "#0066ff"
         }else if(routeShading >= .2 && routeShading < .4){
